3dc77b0948512fbec7a11275805349fb2201f858
Ed Finkler <coj@funkatron.com>
2014-03-30 12:21:06 -0400

    implement api route for GET /api/suggestions

--- app/routes/api.php
+++ app/routes/api.php
@@ -1,9 +1,21 @@
 <?php
 
-$app->group('api', function () use ($app) {
+use \Model as ParisModel;
 
-    $app->get('/', function () use ($app) {
-        echo "hi!";
+$app->group('/api', function () use ($app) {
+
+    $app->group('/suggestions', function () use ($app) {
+
+        $app->get('/', function () use ($app) {
+            $sugg = ParisModel::factory('\GLLApp\Model\Suggestion');
+            $suggestions = $sugg->find_many();
+            $resp_json = [];
+            foreach ($suggestions as $suggestion) {
+                $sugg_arr = $suggestion->as_array();
+                $sugg_arr['type'] = $suggestion->type()->find_one()->as_array();
+                $resp_json[] = $sugg_arr;
+            }
+            $app->setJSONBody($resp_json, 200);
+        });
     });
-
 });
